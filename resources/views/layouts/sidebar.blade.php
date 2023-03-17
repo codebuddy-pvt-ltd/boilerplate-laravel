34fd25c2aba4b867b5247c24c53f83c63bdf884f
Manish Gupta <manish@codebuddy.co>
2023-03-17 10:36:36 +0530

    feat: update stubs for sidebar and menu item

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,55 +1,8 @@
 <div data-simplebar class="h-100">
     <div id="sidebar-menu">
         <ul class="metismenu list-unstyled" id="side-menu">
-
             @include('layouts.menu')
-
         </ul>
-        {{-- <ul class="metismenu list-unstyled" id="side-menu">
-
-            <li>
-                <a href="index.html" class="waves-effect">
-                    <img alt="Dashboard" src="assets/images/dashboard1.svg" class="default-icon">
-                    <img alt="Dashboard" src="assets/images/dashboard2.svg" class="hover-icon">
-                    <span key="t-dashboards">Dashboard</span>
-                </a>
-
-            </li>
-
-            <li>
-                <a href="javascript: void(0);" class="has-arrow waves-effect">
-                    <img alt="Masters" src="assets/images/masters1.svg" class="default-icon">
-                    <img alt="Masters" src="assets/images/masters2.svg" class="hover-icon">
-                    <span key="t-email">Masters</span>
-                </a>
-                <ul class="sub-menu" aria-expanded="false">
-                    <li><a href="product-type.html">Product type</a></li>
-                    <li><a href="brands.html">Brands</a></li>
-                </ul>
-            </li>
-
-            <li>
-                <a href="javascript: void(0);" class="has-arrow waves-effect">
-                    <img alt="Distributors" src="assets/images/distributors1.svg" class="default-icon">
-                    <img alt="Distributors" src="assets/images/distributors2.svg" class="hover-icon">
-                    <span key="t-email">Distributors</span>
-                </a>
-                <ul class="sub-menu" aria-expanded="false">
-                    <li><a href="distributors.html">Personal Details</a></li>
-
-                </ul>
-            </li>
-
-            <li>
-                <a href="form-elements.html" class="waves-effect">
-                    <img alt="Distributors" src="assets/images/credit-card1.svg" class="default-icon">
-                    <img alt="Distributors" src="assets/images/credit-card2.svg" class="hover-icon">
-
-                    <span key="t-file-invoices">Subscription plans</span>
-                </a>
-            </li>
-
-        </ul> --}}
     </div>
     <div class="btm-profile-section">
         <div class="dropdown d-inline-block">
